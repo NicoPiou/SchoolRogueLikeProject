48cf4a991e45a5c5221ac2e52201ea92ab1ca423
NicolasPelicant <niju76290@gmail.com>
2018-04-24 10:48:53 +0200

    J'ai codé le changement de salle

---
+++ Assets/Scenes/Nico/sceneCharger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneCharger : MonoBehaviour
+{
+
+	private int levelNumber;
+
+	public string testActivateNewScene;
+
+
+	void Update()
+	{
+			
+		if(Input.GetButton(testActivateNewScene)){
+			levelNumber = Random.Range (1, 3);
+			Application.LoadLevel ("Room" + levelNumber);
+
+
+	
+}
+}
+}
+
+
